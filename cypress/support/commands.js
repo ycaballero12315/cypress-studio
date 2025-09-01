ab38c1869a478998269b6db1683a77d8613668a3
= <=>
2025-09-01 20:20:24 -0300

    Add Cypress real-events

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,11 +1,11 @@
-
+import 'cypress-real-events'
 Cypress.Commands.add('writer', (selector, text) => {
   cy.get(selector, { timeout: 5000 }).should('exist').clear().type(text);
   cy.wait(500);
 });
 
 Cypress.Commands.add('myclick', (selector) => {
-  cy.get(selector).click();
+  cy.get(selector).realClick();
   cy.wait(500);
 });
 
