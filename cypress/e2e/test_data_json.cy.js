e3ca33ba26d76acd91f373dcde970694a930c42b
= <=>
2025-08-31 22:43:07 -0300

    Ajuste fino de la prueba automatica

--- cypress/e2e/test_data_json.cy.js
+++ cypress/e2e/test_data_json.cy.js
@@ -1,31 +1,28 @@
 /// <reference types="Cypress" />
 
-describe('Test con Studio Cypress', () => {
-let mydata  // variale global que guarda los datos obtenidos del archivo
+describe('Studio Cypress', () => {
+let data 
 
-  beforeEach('Conexión a Institutoweb Login', () => {
-    cy.visit('https://institutoweb.com.ar/test/login.html')
-    cy.fixture('./aleatory_user')  // conectar con el archivo
-    .then(test =>{  // cuando llegan los datos
-        mydata = test // lo que llegó lo paso a mydata 
+  beforeEach('Connection to Institutoweb Login', () => {
+    cy.visit('https://institutoweb.com.ar/elm/login.html')
+    cy.fixture('./aleatory_user') 
+    .then(elm =>{ 
+        data = elm 
     })
   })
 
-  it('Test Login Usuario', () => {
+  it('Login element', () => {
     
-    mydata.forEach((data) =>{  // leo un renglón de los 10 que contiene el json
-          /* ==== Generated with Cypress Studio ==== */
+    data.forEach((data) =>{
+        
           cy.get('#tuusuario').type(data.user);
           cy.get('#tuclave').type(data.pass);
           cy.get('#tumail').type(data.email);
-          //cy.wait(2000)
-          // click del botón ingresar
+
           cy.get(':nth-child(8)').click();
-          // ingresamos en la segunda pantalla
-          // Aserción del Título
-          cy.get('h3').should('have.text', 'Acceso correcto!');
+
+          cy.get('h3').should('have.text', 'Corret!');
           cy.get('#volver').click();
-          /* ==== End Cypress Studio ==== */
     })   
   })
 
